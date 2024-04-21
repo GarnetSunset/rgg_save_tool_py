de17e049f295b210a2f52d7eb2765482372d74e5
James Duarte <GarnetSunset@users.noreply.github.com>
2024-04-20 22:20:03 -0400

    Latest RGG Save tool, a complete rewrite.

--- rgg_save_tool.py
+++ rgg_save_tool.py
@@ -1,5 +1,3 @@
-import chardet
-import json
 import os
 import sys
 import zlib
@@ -31,42 +29,44 @@ game_abbr_to_name = {
 }
 
 # Headers for automatic detection
-gaiden_headers = [
-    b"\x11\x69\x63\x27\x20\x04\x15\x69\x01\x5f",
-    b"\x11\x69\x63\x27\x20\x04\x15\x69\x02\x40",
-]
-ik_headers = [
-    b"\x72\x75\x45\x77\x21\x72\x57\x4e\x2c\x4d",
-    b"\x60\x75\x45\x77\x22\x72\x57\x4e\x2c\x4d",
-]
-je_headers = [
-    b"\x34\x52\x46\x2f\x0b\x08\x40\x6a\x5a\x7a",
-    b"\x34\x52\x46\x2f\x0b\x08\x40\x6a\x5d\x62",
-]
-lj_headers = [
-    b"\x11\x69\x63\x27\x20\x04\x15\x69\x00\x54",
-    b"\x11\x69\x63\x27\x20\x04\x15\x69\x02\x40",
-]
-y6_headers = [
-    b"\x2d\x6b\x1d\x04\x07\x22\x41\x51\x7f\x43",
-    b"\x2d\x6b\x1d\x04\x07\x22\x41\x51\x7c\x5f",
-]
-y7_headers = [
-    b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x00\x72",
-    b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x07\x68",
-]
-y7_gog_headers = [
-    b"\x09\x11\x6a\x3a\x54\x43\x71\x6e\x52\x44",
-    b"\x09\x11\x6a\x3a\x54\x43\x71\x6e\x55\x5e",
-]
-yk2_headers = [
-    b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x02\x73",
-    b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x00\x68",
-]
-y8_headers = [
-    b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x06\x73",
-    b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x05\x68",
-]
+game_headers = {
+    "gd": [
+        b"\x11\x69\x63\x27\x20\x04\x15\x69\x01\x5f",
+        b"\x11\x69\x63\x27\x20\x04\x15\x69\x02\x40",
+    ],
+    "ik": [
+        b"\x72\x75\x45\x77\x21\x72\x57\x4e\x2c\x4d",
+        b"\x60\x75\x45\x77\x22\x72\x57\x4e\x2c\x4d",
+    ],
+    "je": [
+        b"\x34\x52\x46\x2f\x0b\x08\x40\x6a\x5a\x7a",
+        b"\x34\x52\x46\x2f\x0b\x08\x40\x6a\x5d\x62",
+    ],
+    "lj": [
+        b"\x11\x69\x63\x27\x20\x04\x15\x69\x00\x54",
+        b"\x11\x69\x63\x27\x20\x04\x15\x69\x02\x40",
+    ],
+    "y6": [
+        b"\x2d\x6b\x1d\x04\x07\x22\x41\x51\x7f\x43",
+        b"\x2d\x6b\x1d\x04\x07\x22\x41\x51\x7c\x5f",
+    ],
+    "y7": [
+        b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x00\x72",
+        b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x07\x68",
+    ],
+    "y7_gog": [
+        b"\x09\x11\x6a\x3a\x54\x43\x71\x6e\x52\x44",
+        b"\x09\x11\x6a\x3a\x54\x43\x71\x6e\x55\x5e",
+    ],
+    "yk2": [
+        b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x02\x73",
+        b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x00\x68",
+    ],
+    "y8": [
+        b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x06\x73",
+        b"\x28\x76\x4f\x04\x3c\x28\x45\x48\x05\x68",
+    ],
+}
 
 
 def xor_data(data, key):
@@ -74,99 +74,34 @@ def xor_data(data, key):
     return bytearray((b ^ ord(key[i % key_len])) for i, b in enumerate(data))
 
 
-def get_file_encoding(data):
-    detected = chardet.detect(data)
-    return detected["encoding"] if detected["encoding"] else "utf-8"
-
-
-def modify_json_binary(data, modifications):
-    try:
-        detected = chardet.detect(data)
-        encoding = detected["encoding"] if detected["encoding"] else "utf-8"
-        data_str = data.decode(encoding, errors="ignore")
-
-        if not data_str.strip().startswith(("{", "[")):
-            raise ValueError("Decoded data does not appear to be valid JSON")
-
-        data_json = json.loads(data_str)
-
-        for key_path, value in modifications.items():
-            keys = key_path.split(".")
-            temp = data_json
-            for key in keys[:-1]:
-                temp = temp.setdefault(key, {})
-            temp[keys[-1]] = value
-
-        modified_str = json.dumps(data_json, ensure_ascii=False)
-        return modified_str.encode(encoding)
-    except json.JSONDecodeError as e:
-        print(f"Error processing JSON data: {e}")
-        return data
-
-
 def crc32_checksum(data):
     return zlib.crc32(data) & 0xFFFFFFFF
 
 
-def read_game_version_from_file(filename):
-    with open(filename, "r", encoding="utf-8") as file:
-        data = json.load(file)
-        return data["base"]["m_version"]
-
-
-def process_file(filename, game, example_save_file=None, encrypt=False):
-    modifications = {}
-    if example_save_file and not encrypt:
-        # If there's an example save file and we're decrypting, read the version from it
-        target_version = read_game_version_from_file(example_save_file)
-        modifications["base.m_version"] = (
-            target_version  # Specify the nested path for the modification
-        )
-        print("Got example.")
-
+def process_file(filename, game, encrypt=False):
     key = game_keys.get(game)
     if not key:
         print(f"Unsupported game: {game}")
         return
+
+    base, ext = os.path.splitext(filename)
+    if encrypt:
+        outname = f"{base.split('_')[0]}.sav"  # Restore to original .sav name
+    else:
+        outname = f"{base}_{game}.json"  # Append game abbreviation for JSON
 
     try:
         with open(filename, "rb") as in_file:
             data = in_file.read()
 
         if encrypt:
-            # Decrypt the data first if it's already in an encrypted state
-            if game in [
-                "ik",
-                "ishin",
-            ]:  # Special handling for specific games, if needed
-                data = xor_data(
-                    data[:-8], key
-                )  # Adjust if your game's logic is different
-            else:
-                data = xor_data(
-                    data[:-4], key
-                )  # Standard decryption, removing checksum
-
-            # Convert decrypted data to JSON and modify
-            if modifications:
-                data = modify_json_binary(data, modifications)
-
-            # Re-encrypt the modified data
-            data = xor_data(data, key)
-            # Append the checksum back to the data
-            checksum = crc32_checksum(data)
-            data += checksum.to_bytes(4, byteorder="little")
-
+            data = xor_data(data, key) + crc32_checksum(data).to_bytes(
+                4, byteorder="little"
+            )
         else:
-            # Standard decryption logic
-            data = xor_data(data[:-4], key)  # Adjust according to your game's specifics
-
-            # If we need to modify the JSON after decryption
-            if modifications:
-                data = modify_json_binary(data, modifications)
-
-        # Determine the output filename based on whether we're encrypting or decrypting
-        outname = filename + (".json" if not encrypt else ".sav")
+            data = xor_data(
+                data[:-4], key
+            )  # Remove checksum assumed to be last 4 bytes
 
         with open(outname, "wb") as out_file:
             out_file.write(data)
@@ -177,86 +112,48 @@ def process_file(filename, game, example_save_file=None, encrypt=False):
         print(f"Error processing {filename}: {e}")
 
 
-def is_game_save(file_path, headers):
-    with open(file_path, "rb") as file:
-        file_header = file.read(10)  # Assuming headers are 10 bytes
-    return any(file_header == header for header in headers)
+def is_game_save(file_path, game):
+    try:
+        with open(file_path, "rb") as file:
+            file_header = file.read(10)  # Read first 10 bytes
+        return any(file_header.startswith(header) for header in game_headers[game])
+    except KeyError:
+        return False
 
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python script.py [file] [optional: game number]")
+        print("Usage: python script.py [file] [optional: game abbreviation]")
         return
 
     filename = sys.argv[1]
     extension = os.path.splitext(filename)[1].lower()
 
-    # Try automatic game detection first
-    game_abbr = sys.argv[2] if len(sys.argv) > 2 else None
-    if game_abbr and game_abbr in game_abbr_to_name:
-        print(f"Game selected: {game_abbr_to_name[game_abbr]}")
-    else:
-        if is_game_save(filename, gaiden_headers):
-            game_abbr = "gd"
-        elif is_game_save(filename, y7_headers):
-            game_abbr = "y7"
-        elif is_game_save(filename, y7_gog_headers):
-            game_abbr = "y7_gog"
-        elif is_game_save(filename, y8_headers):
-            game_abbr = "y8"
-        elif is_game_save(filename, yk2_headers):
-            game_abbr = "yk2"
-        elif is_game_save(filename, lj_headers):
-            game_abbr = "lj"
-        elif is_game_save(filename, je_headers):
-            game_abbr = "je"
-        elif is_game_save(filename, ik_headers):
-            game_abbr = "ik"
-        elif is_game_save(filename, y6_headers):
-            game_abbr = "y6"
+    # Attempt to detect game from filename
+    game_abbr = None
+    for key in game_keys:
+        if f"_{key}." in filename:
+            game_abbr = key
+            break
 
-    example_save_file = sys.argv[3] if len(sys.argv) > 3 else None
-
-    # If game_abbr is None or not a valid key, ask the user to enter the game manually
-    if not game_abbr or game_abbr not in game_abbr_to_name:
-        print("Game not detected automatically or invalid game abbreviation provided.")
-        print("Select the game:")
-        for idx, game_name in enumerate(game_abbr_to_name.values(), start=1):
-            print(f"{idx}. {game_name}")
-
-        try:
-            game_idx = int(input("Enter the game number: ")) - 1
-            if game_idx < 0 or game_idx >= len(game_abbr_to_name):
-                print("Invalid selection. Exiting.")
-                return
-            game_abbr = list(game_abbr_to_name.keys())[game_idx]
-        except ValueError:
-            print("Invalid input. Exiting.")
-            return
-    else:
-        print(f"Game is: {game_abbr_to_name[game_abbr]}")
-
-    if extension == ".json" and game_abbr != "ik" and game_abbr:
-        with open(filename, "r", encoding="utf-8") as f:
-            dict = json.load(f)
-        if "rggsc_game_identifier" in dict:
-            game_abbr = dict["rggsc_game_identifier"]
-
-    # If game is not detected automatically, ask the user to choose
+    # If not found in filename, try detecting from file header
     if not game_abbr:
-        print("Select the game:")
-        for idx, game_name in enumerate(game_abbr_to_name.values(), start=1):
-            print(f"{idx}. {game_name}")
+        for game, headers in game_headers.items():
+            if is_game_save(filename, game):
+                game_abbr = game
+                print(f"Detected game based on file header: {game_abbr_to_name[game]}")
+                break
 
-        game_idx = int(input("Enter the game number: ")) - 1
-        game_abbr = list(game_abbr_to_name.keys())[game_idx]
+    if not game_abbr:
+        print("Failed to detect game automatically. Please specify game abbreviation.")
+        return
 
-    if extension in (".sav", ".sys"):
-        process_file(filename, game_abbr, example_save_file)
-    elif extension == ".json":
-        process_file(filename, game_abbr, example_save_file, encrypt=True)
+    if extension == ".json":
+        process_file(filename, game_abbr, encrypt=True)
+    elif extension in (".sav", ".sys"):
+        process_file(filename, game_abbr)
     else:
-        print(f"Skipping {filename}: Unsupported file type.")
+        print(f"Unsupported file type for {filename}.")
 
 
 if __name__ == "__main__":
